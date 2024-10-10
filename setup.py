328d86936d7ea10ae41413de4962538c051d1aa2
WillSH97 <williamsh1997@hotmail.com>
2024-10-10 11:08:04 +1000

    :clown_face: some python package bullshit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="SD_utils",
+    name="SDutils",
     version="0.0.1",
     author="QUT GenAI Lab",
     author_email="william.he@qut.edu.au",
